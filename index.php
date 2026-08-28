8ed569051e894093b17647a840d2a77e0f32c370
Dante Mita <dantemp123n@gmail.com>
2026-08-28 10:14:03 -0500

    prueba 2 con un index con php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- Index de una Tienda virtual -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MENU</title>
+</head>
+<body>
+    <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Contacto</li>
+        <li>Carrito</li>
+        <li>Login</li>
+        <li> //carrito </li>
+    </ul>
+</body>
+</html>
